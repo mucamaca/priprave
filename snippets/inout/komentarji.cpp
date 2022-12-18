12bf983477951376ba1917d9841d50a2b35b4c39
Patrik Žnidaršič <posta.patrik.2@gmail.com>
2022-12-18 18:13:37 +0100

    Poglavje inout v novem template sistemu

---
+++ snippets/inout/komentarji.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){ //komentar do konca vrstice
+    printf /*komentar znotraj vrstice*/("Zivjo svet!\n");
+    /*
+    komentar
+    čez
+    več
+    vrstic
+    */
+    return 0;
+}
